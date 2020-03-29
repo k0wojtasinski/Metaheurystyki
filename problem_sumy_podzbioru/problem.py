dada8be036a6b708adb41e981275b3592436f5ea
Kacper Wojtasiński <k0wojtasinski@gmail.com>
2020-03-29 15:51:13 +0200

    Added comparison of solutions

--- problem_sumy_podzbioru/problem.py
+++ problem_sumy_podzbioru/problem.py
@@ -13,13 +13,42 @@ logging.basicConfig(format=FORMAT)
 logger = logging.getLogger("Problem")
 logger.setLevel("INFO")
 
+
 class Solution(abc.ABC):
+    """ abstract class representing single solution
+        provides comparison, is_correct and __str__
+        goal method needs to be implemented
+    """
     def __init__(self, data: dict):
         self.data = data
 
     def __eq__(self, solution: "Solution"):
         if isinstance(solution, self):
             return self.data == solution.data
+
+    def __lt__(self, solution: "Solution"):
+        if self.goal() < 0 and solution.goal() < 0:
+            return False
+        
+        if self.goal() < 0:
+            return False
+        
+        if solution.goal() < 0:
+            return True
+
+        return self.goal() < solution.goal()
+
+    def __gt__(self, solution: "Solution"):
+        if self.goal() < 0 and solution.goal() < 0:
+            return False
+        
+        if self.goal() < 0:
+            return True
+        
+        if solution.goal() < 0:
+            return False
+
+        return self.goal() > solution.goal()
 
     @abc.abstractmethod
     def goal(self) -> int:
@@ -33,6 +62,7 @@ class Solution(abc.ABC):
 
     def __repr__(self):
         return str(self)
+
 
 class Problem:
     def __init__(self, data: dict):
@@ -53,8 +83,10 @@ class Problem:
             results = []
             for item in content:
                 results.append(cls(item))
-        
-        logger.info(f"Loading input from json file (file_name: {file_path}, content: {content})")
+
+        logger.info(
+            f"Loading input from json file (file_name: {file_path}, content: {content})"
+        )
         return results
 
     def __str__(self):
@@ -78,14 +110,17 @@ class Problem:
 
     def export_correct_solutions_to_json(self, file_path):
         """ method to export all correct solutions to JSON file """
-        solutions = [solution.data for solution in self.solutions if solution.is_correct()]
+        solutions = [
+            solution.data for solution in self.solutions if solution.is_correct()
+        ]
         self.export_solutions_to_json(solutions, file_path)
 
     def export_incorrect_solutions_to_json(self, file_path):
         """ method to export all incorrect solutions to JSON file """
-        solutions = [solution.data for solution in self.solutions if not solution.is_correct()]
+        solutions = [
+            solution.data for solution in self.solutions if not solution.is_correct()
+        ]
         self.export_solutions_to_json(solutions, file_path)
-
 
 
 class SumOfSubsetSolution(Solution):
@@ -94,18 +129,19 @@ class SumOfSubsetSolution(Solution):
         self.set = data["set"]
         self.subset = data["subset"]
         self.number = data["number"]
-        self.tries = self.data["tries"]
-        self.time = self.data["time"]
+        self.tries = self.data.get("tries", 0)
+        self.time = self.data.get("time", 0)
 
     def goal(self) -> int:
         if self.check_correctness(self.set, self.subset, self.number):
             return abs(sum(self.subset) - self.number)
-        
+
         return -1
 
     @staticmethod
     def check_correctness(set_of_numbers, subset, number) -> bool:
         return all([number in set_of_numbers for number in subset])
+
 
 class SumOfSubsetProblem(Problem):
     def __init__(self, data):
@@ -121,22 +157,28 @@ class SumOfSubsetProblem(Problem):
 
             for combination in itertools.combinations(self.set, i):
 
-                solution = SumOfSubsetSolution({
-                "number": self.number,
-                "subset": combination,
-                "set": self.set,
-                "tries": len(self.solutions),
-                "time": time.time() - start_time
-            })
+                solution = SumOfSubsetSolution(
+                    {
+                        "number": self.number,
+                        "subset": combination,
+                        "set": self.set,
+                        "tries": len(self.solutions),
+                        "time": time.time() - start_time,
+                    }
+                )
                 solution.data["tries"] = len(self.solutions)
-                
+
                 self.solutions.append(solution)
 
                 if solution.is_correct():
-                    logger.info(f"Found solution (time={solution.time}, tries={solution.tries})")
+                    logger.info(
+                        f"Found solution (time={solution.time}, tries={solution.tries})"
+                    )
                     return solution
 
         end_time = time.time() - start_time
-        logger.warn(f"Solution cannot be found (time={end_time}, tries={len(self.solutions)})")
-        
+        logger.warn(
+            f"Solution cannot be found (time={end_time}, tries={len(self.solutions)})"
+        )
+
         return []
